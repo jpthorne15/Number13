2407ff234041c52923de71533201bfbb35fd9305
Jim Thorne <jamesthorne@Jamess-MacBook-Air.local>
2020-01-07 12:08:43 -0700

    first commit

---
+++ number13.js
@@ -0,0 +1,14 @@
+// Logical AND operator '&&'
+let result = true && true;
+console.log (result);
+// expected result true
+
+/* To get the result 'true' both operands have to be true.*/
+
+result = true && false;
+console.log (result);
+// expected result false
+
+result = false && true;
+console.log (result);
+// expected result false
